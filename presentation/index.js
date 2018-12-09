ff779f721031e72c783fd6e938afdfcabc3b49cc
Joakim Sælemyr <joakim.saelemyr@gmail.com>
2018-12-09 22:57:06 +0100

    more slides

--- presentation/index.js
+++ presentation/index.js
@@ -72,10 +72,10 @@ export default class Presentation extends React.Component {
           <HeadingMedium>Hvordan følge dagens workshop</HeadingMedium>
           <br/>
           <HeadingSmallLeft>Uten oppsett:</HeadingSmallLeft>
-          <TextSmallLeft><Link href="https://codesandbox.io">https://codesandbox.io</Link></TextSmallLeft>
+          <TextSmallLeft><Link href="https://codesandbox.io" target="_blank">https://codesandbox.io</Link></TextSmallLeft>
           <br/>
           <HeadingSmallLeft>Kjøre lokalt:</HeadingSmallLeft>
-          <TextSmallLeft>Installer <CodeSmall>nodejs</CodeSmall>. Fås på: <Link href="https://nodejs.org">https://nodejs.org</Link></TextSmallLeft>
+          <TextSmallLeft>Installer <CodeSmall>nodejs</CodeSmall>. Fås på: <Link href="https://nodejs.org" target="_blank">https://nodejs.org</Link></TextSmallLeft>
           <br/>
           <TextSmallLeft>Så kjør i en terminal:</TextSmallLeft>
           <CodePane margin="20px auto" style={{fontSize: "1.2rem"}} lang="javascript" source={require('raw-loader!../assets/nodejs-install.example')}/>
@@ -124,7 +124,7 @@ export default class Presentation extends React.Component {
             </ol>
           </Notes>
           <HeadingLarge caps={false}>Overordnet</HeadingLarge>
-<List>
+          <List>
             <ListItem>Fleksibelt</ListItem>
             <ListItem>Ingen spesiell syntax (nesten)</ListItem>
             <ListItem>Komponentbasert</ListItem>
@@ -154,8 +154,140 @@ export default class Presentation extends React.Component {
         </Slide>
 
         <Slide transition={["fade"]} bgColor="primary" textColor="primary">
-          <HeadingLarge>Hello world</HeadingLarge>
-          en komponent her, så se på JSX 
+          <Notes>
+            <h4>Notes</h4>
+            <ol>
+              <li>eksempel på komponent</li>
+              <li>klasser har render fn</li>
+              <li>javascript rundt, jsx inni. ingen '' rundt html'en</li>
+            </ol>
+          </Notes>
+          <CodePane margin="20px auto" style={{fontSize: "1.2rem"}} lang="javascript" source={require('raw-loader!../assets/react-helloworld.example')}/>
+        </Slide>
+
+        <Slide transition={["fade"]} bgColor="primary" textColor="secondary">
+          <Notes>
+            <h4>Notes</h4>
+            <ol>
+              <li>hvordan ser det ut?</li>
+            </ol>
+          </Notes>
+          <HeadingMedium size={1}>JSX</HeadingMedium>
+          <List>
+            <ListItem>Utvidelse av js syntax</ListItem>
+            <ListItem>Ikke en string av html</ListItem>
+            <ListItem>Heller ikke html</ListItem>
+          </List>
+        </Slide>
+
+        <Slide transition={["fade"]} bgColor="primary" textColor="secondary">
+          <CodePane margin="20px auto" style={{fontSize: "1.2rem"}} lang="javascript" source={require('raw-loader!../assets/jsx.example')} />
+        </Slide>
+
+        <Slide transition={["fade"]} bgColor="primary" textColor="secondary">
+          <HeadingLarge>Flere måter å lage en komponent</HeadingLarge>
+        </Slide>
+
+        <Slide transition={["fade"]} bgColor="primary" textColor="secondary">
+          <Notes>
+            <h4>Notes</h4>
+            <ol>
+              <li>to hovedkategorier</li>
+              <li>funksjonelle og klasse</li>
+              <li>klasse for lokal state</li>
+              <li>funksjonell når man ikke trenger lokalt state</li>
+              <li>javascript syntaks tillater flere måter, es5 6 etc</li>
+            </ol>
+          </Notes>
+          <CodePane margin="20px auto" style={{fontSize: "1.2rem"}} lang="javascript" source={require('raw-loader!../assets/react-components.example')} />
+        </Slide>
+
+        <Slide transition={["fade"]} bgColor="secondary" textColor="primary">
+          <HeadingLarge caps={false}>Oppgaverunde</HeadingLarge>
+          <br />
+          <List ordered textSize="1rem" textAlign="left">
+            <ListItem textSize="2.2rem">Lag noen React komponenter. Prøv både klasse- og funksjonskomponenter</ListItem>
+            <br />
+            <ListItem textSize="2.2rem">Prøv å lage en React komponent som bruker litt forskjellige HTML tag'er'</ListItem>
+            <br />
+            <ListItem textSize="2.2rem">Bonus: Ta i bruk èn av komponenten deres i en annen.</ListItem>
+          </List>
+        </Slide>
+
+        <Slide transition={["fade"]} bgColor="secondary" textColor="primary">
+          <HeadingLarge>State</HeadingLarge>
+        </Slide>
+
+        <Slide transition={["fade"]} bgColor="secondary" textColor="primary">
+          <Notes>
+            <h4>Notes</h4>
+            <ol>
+              <li>En gammel/vanlig. Konstruktør måten</li>
+              <li>Ny, mindre vanlig. JavaScript class fields</li>
+              <li>Brukes til lokal tilstand</li>
+            </ol>
+          </Notes>
+          <HeadingLarge>2 måter å sette State</HeadingLarge>
+          <CodePane margin="20px auto" style={{fontSize: "1.2rem"}} lang="javascript" source={require('raw-loader!../assets/react-state1.example')} />
+        </Slide>
+
+        <Slide transition={["fade"]} bgColor="secondary" textColor="primary">
+          <Notes>
+            <h4>Notes</h4>
+            <ol>
+              <li>React eier props. Ødelegger React sin state ved å endre</li>
+            </ol>
+          </Notes>
+          <HeadingLarge size={2} textColor="tertiary" caps>Props</HeadingLarge>
+          <br/>
+          <br/>
+          <HeadingMedium size={4} textColor="primary" >For å sende inn data til en komponent</HeadingMedium>
+          <HeadingMedium size={4} textColor="primary" >Immutable, ikke lov å endre</HeadingMedium>
+        </Slide>
+
+        <Slide transition={["fade"]} bgColor="secondary" textColor="primary">
+          <CodePane margin="20px auto" style={{fontSize: "1.2rem"}} lang="javascript" source={require('raw-loader!../assets/react-props.example')} />
+        </Slide>
+
+        <Slide transition={["fade"]} bgColor="secondary" textColor="primary">
+          <HeadingLarge>PropTypes</HeadingLarge>
+          <HeadingSmall textColor="primary">Måte å beskrive data en komponent benytter</HeadingSmall>
+          <br/>
+          <HeadingSmall textColor="primary">Forskjellige typer props</HeadingSmall>
+          <br/>
+          <HeadingSmall textColor="primary">Required/Optional</HeadingSmall>
+        </Slide>
+
+        <Slide transition={["fade"]} bgColor="secondary" textColor="primary">
+          <Notes>
+            <h4>Notes</h4>
+            <ol>
+              <li>Er egen pakke</li>
+              <li>Gir error i console når mangler</li>
+              <li>Funksjoner er objekter i praksis. Kan sette properties på de</li>
+            </ol>
+          </Notes>
+          <CodePane margin="20px auto" style={{fontSize: "1.2rem"}} lang="javascript" source={require('raw-loader!../assets/react-proptypes.example')} />
+        </Slide>
+
+        <Slide transition={["fade"]} bgColor="secondary" textColor="primary">
+          <Notes>
+            <h4>Notes</h4>
+            <ol>
+              <li>Ta en oppsummeringsrunde</li>
+            </ol>
+          </Notes>
+          <HeadingLarge caps={false}>Oppgaverunde</HeadingLarge>
+          <br />
+          <List ordered textSize="1rem" textAlign="left">
+            <ListItem textSize="2.2rem">Lag en komponent med initiell state</ListItem>
+            <br />
+            <ListItem textSize="2.2rem">La komponenten få inn noe via props</ListItem>
+            <br />
+            <ListItem textSize="2.2rem">Bruk komponeten og test å sende inn forskjellige verdier</ListItem>
+            <br />
+            <ListItem textSize="2.2rem">Lag PropTypes for komponten. Bonus: Åpne console'n og se at man får error log når prop ikke er tilstede</ListItem>
+          </List>
         </Slide>
       </Deck>
     );
